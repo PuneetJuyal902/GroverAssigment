fe4d7b10d282a2a367f79c3612bdcd3723770e63
PuneetJuyal902 <89142365+PuneetJuyal902@users.noreply.github.com>
2021-08-20 16:55:57 +0200

    Add files via upload
    added_cases_and_framework

---
+++ core/src/main/java/coreFramework/base/JsonReader.java
@@ -0,0 +1,57 @@
+package coreFramework.base;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+public class JsonReader {
+	
+		
+	public static Object[][] getdata(String JSON_path, String typeData, int totalDataRow, int totalColumnEntry) {
+		JsonParser jsonParser =  new JsonParser();
+		JsonObject jsonObj = null;
+		
+		try {
+			jsonObj = jsonParser.parse(new FileReader(JSON_path)).getAsJsonObject();
+		} catch (JsonIOException e) {
+			
+			e.printStackTrace();
+		} catch (JsonSyntaxException e) {
+			
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		JsonObject obj = (JsonObject) jsonObj.getAsJsonObject(typeData);
+        return searchJsonElement(obj, totalDataRow, totalColumnEntry);
+	}
+	
+	public static Object[][] searchJsonElement(JsonObject jsonObj, int totalDataRow, int totalColumnEntry) 
+	{			
+		Object[][] matrix = new Object[totalDataRow][totalColumnEntry];
+        int i =0;
+        int j = 0;
+        for (String jsonElement : jsonObj.keySet()) 
+        	
+        {
+        	String val = jsonObj.get(jsonElement).toString();
+            	 matrix[i][j] = val.replace("\"","");
+                 j++;   
+            }
+            i++;
+            j = 0;
+        
+      return matrix;
+	}
+}
+
+
+
---
+++ core/src/main/java/coreFramework/base/LocalBrowserInitialzation.java
@@ -0,0 +1,57 @@
+package coreFramework.base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;	
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LocalBrowserInitialzation {
+	
+	public static WebDriver driver=null;
+	public static  WebDriver setup(String url, String browserName){
+		
+		if (driver == null) {
+			switch (browserName) {
+			case "FireFox":
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+				driver.get(url);
+				driver.manage().window().maximize();
+				break;
+
+			case "Chrome":
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				driver.get(url);
+				driver.manage().window().maximize();
+				
+				break;
+			case "IE":
+				WebDriverManager.iedriver().setup();
+				driver = new InternetExplorerDriver();
+				driver.get(url);
+				driver.manage().window().maximize();
+				
+				break;
+				
+					
+			default:
+				throw new IllegalArgumentException(
+						String.format("%s browser is not valid please provide valid browserName", browserName));
+			}
+		}
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	public static void quitDriver() {
+		if (driver != null) {
+			driver.quit();
+			driver=null;
+
+		}
+	}
+}
+	
+	
+
